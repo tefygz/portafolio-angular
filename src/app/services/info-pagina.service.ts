275d06e90f198fd1638e0df43e8aa9cb5b532ccc
Estefania Gonzalez <tefys.gz@gmail.com>
2020-05-12 17:02:27 -0300

    Fin de la sección 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,17 +10,32 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
+  equipo: any [] = [];
+
   constructor(private http: HttpClient) { 
 
-   // console.log('Servicio listo');
+    this.cargarInfo();
+    this.cargarEquipo();
 
-    // leerel json
+    
+
+  }
+  private cargarInfo(){
     this.http.get('assets/data/data-pagina.json')
     .subscribe( (resp: InfoPagina) => {
       this.cargada = true;
       this.info = resp;
       console.log(resp);
     });
+  }
+  private cargarEquipo(){
 
+    this.http.get('https://angular-html-f5210.firebaseio.com/equipo.json')
+    .subscribe( (resp: any)  => {
+     
+      this.equipo = resp;
+      console.log(resp);
+    });
+    
   }
 }
