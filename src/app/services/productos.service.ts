31fcb7d115ba9722baceee6d4f171a1e9b5b5d9b
Estefania Gonzalez <tefys.gz@gmail.com>
2020-05-14 18:52:03 -0300

    listo para produccion

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Producto } from '../interfaces/producto.interface';
-import { resolve } from 'dns';
-import { rejects } from 'assert';
+
 
 
 @Injectable({
@@ -21,6 +20,7 @@ export class ProductosService {
 
   private cargarProductos(){
 
+    // tslint:disable-next-line: no-shadowed-variable
     return new Promise ((resolve, reject ) => {
 
       this.http.get('https://angular-html-f5210.firebaseio.com/productos_idx.json')
