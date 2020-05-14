8090e3cdf0429987ef664fa9e6f6f426f2850fc2
Estefania Gonzalez <tefys.gz@gmail.com>
2020-05-14 18:04:53 -0300

    Fin sección 7

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Producto } from '../interfaces/producto.interface';
+import { resolve } from 'dns';
+import { rejects } from 'assert';
 
 
 @Injectable({
@@ -10,6 +12,7 @@ export class ProductosService {
 
   cargando = true;
   productos: Producto[] = [];
+  productosFiltrados: Producto[] = [];
 
   constructor( private http: HttpClient) { 
 
@@ -17,15 +20,51 @@ export class ProductosService {
   }
 
   private cargarProductos(){
-  
-    this.http.get('https://angular-html-f5210.firebaseio.com/productos_idx.json')
+
+    return new Promise ((resolve, reject ) => {
+
+      this.http.get('https://angular-html-f5210.firebaseio.com/productos_idx.json')
     .subscribe( (resp: Producto[]) =>{
-      console.log(resp);
       this.productos = resp;
       this.cargando = false;
+      resolve();
     });
+    });
+  
+    
   }
 
+ getProducto(id: string){
 
+  return this.http.get(`https://angular-html-f5210.firebaseio.com/productos/${ id }.json`)
+ }
+
+ buscarProducto( termino: string){
+
+  if(this.productos.length === 0){
+      this.cargarProductos().then(()=>{
+      this.filtrarProductos(termino);
+      });
+  } else {
+      this.filtrarProductos(termino);
+  }
+  
+ }
+
+ private filtrarProductos( termino: string){
+    
+    this.productosFiltrados = [];
+
+    termino = termino.toLocaleLowerCase();
+
+    this.productos.forEach( prod =>{
+
+      const tituloLower = prod.titulo.toLocaleLowerCase();
+
+      if(prod.categoria.indexOf(termino) >= 0 || tituloLower.indexOf(termino) >= 0) {
+          this.productosFiltrados.push(prod);
+        }
+    });
+ }
 
 }
